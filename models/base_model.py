e9343ff4f76011545be4b442ea2b64e37b307941
root <santmendieta@icloud.com>
2020-12-17 18:28:44 -0500

    refactor

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from models import storage
 
 Base = declarative_base()
 
@@ -19,15 +20,13 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.created_at = self.updated_at = datetime.now()
         else:
             if 'id' not in kwargs.keys():
                 kwargs['id'] = str(uuid.uuid4())
-                kwargs['created_at'] = datetime.now().isoformat()
-                kwargs['updated_at'] = datetime.now().isoformat()
+                kwargs['created_at'] = datetime.now()
+                kwargs['updated_at'] = datetime.now()
                 kwargs['__class__'] = self.__class__.__name__
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -38,12 +37,11 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        #cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
         storage.save()
@@ -52,16 +50,14 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
         if '_sa_instance_state' in dictionary.keys():
             del dictionary['_sa_instance_state']
         return dictionary
-    
+
     def delete(self):
         """Instance method to delete current instance from the storage"""
-        from models import storage
         if self in storage.all().values():
-            del storage.all()[self.__class__.__name__ + "." + self.id]
+            storage.delete(self)
--- models/city.py
+++ models/city.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-
-from os import getenv
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
-from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
