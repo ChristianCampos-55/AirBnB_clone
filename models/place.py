9fcce7511a2efcf83fa745c482ab2a5793cd842e
root <santmendieta@icloud.com>
2020-12-17 19:55:13 -0500

    Revert "engine db storaged changed" trying to know requirements This reverts commit dec94bbd732296e57542f1048a35602f9258f89e.

--- models/place.py
+++ models/place.py
@@ -10,19 +10,9 @@ from models.review import Review
 from sqlalchemy.schema import Table
 
 
-
-place_amenity = Table('place_amenity', Base.metadata,
-                      Column('place_id', String(60),
-                             ForeignKey('places.id'),
-                             nullable=False),
-                      Column('amenity_id', String(60),
-                             ForeignKey('amenities.id'),
-                             nullable=False))
-
-
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = "places"
+    __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
@@ -33,35 +23,42 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", cascade="all, delete", backref="place")
     amenity_ids = []
 
-    @property
-    def amenities(self):
-        """ getter method for amenities"""
-        amenities_list = []
-        from models import storage
-        dictionary = storage.all(Amenity)
-        if dictionary:
-            for k, v in dictionary.items():
-                if self.id == v.place_id:
-                    amenities_list.append(v)
-        return amenities_list
+    place_amenity = Table('place_amenity', Base.metadata,
+                          Column('place_id', String(60),
+                                 ForeignKey('places.id'), primary_key=True),
+                          Column('amenity_id', String(60),
+                                 ForeignKey('amenities.id'), nullable=False))
 
-    @amenities.setter
-    def amenities(self, value):
-        """setter method for amenities"""
-        if type(value) != Amenity:
-            return
-        self.amenity_ids.append(value)
+    if os.environ.get("HBNB_TYPE_STORAGE") == "db":
+        amenities = relationship("Amenity", secondary="place_amenity",
+                                 viewonly=False)
+
+    else:
+        @property
+        def amenities(self):
+            """ getter for amenities"""
+            amenity_list = []
+            results = storage.all(Amenity)
+            for amenity in results.values():
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+
+        @amenities.setter
+        def amenities(self, obj):
+            """ amenities setter"""
+            if obj and isinstance(obj, Amenity):
+                type(self).amenity_ids.append(obj.id)
 
     @property
     def reviews(self):
-        """ getter method for reviews"""
-        reviews_list = []
-        from models import storage
-        dictionary = storage.all(Review)
-        if dictionary:
-            for k, v in dictionary.items():
-                if self.id == v.place_id:
-                    reviews_list.append(v)
-        return reviews_list
+        """getter reviews"""
+        review_dict = {}
+        objs_ = storage.all(Review)
+        for key, value in objs_.items():
+            if value.place_id == self.id:
+                review_dict[key] = value
+        return review_dict
