5869a21a45f5ed4932c5c6f228aa21889e2ccadc
root <santmendieta@icloud.com>
2020-12-17 23:57:11 -0500

    Revert "debugging states restated"
    This reverts commit e68911b46ac18301566032c2ed5ea3d1f3a2bf90.

--- models/state.py
+++ models/state.py
@@ -11,11 +11,10 @@ from models.city import City
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="delete")
 
-    elif os.getenv("HBNB_TYPE_STORAGE") != "db":
+    if os.getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """Cities getter to find object """
