d8a6b25d21377710e359438a41f519ae18d30100
root <santmendieta@icloud.com>
2020-12-17 22:49:30 -0500

    debugging states restated

--- models/state.py
+++ models/state.py
@@ -1,24 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage
 from models.city import City
 from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
     @property
     def cities(self):
-        """cities getter"""
-        list_cities = []
+        """ getter method for cities"""
+        from models import storage
+        cities_list = []
         dictionary = storage.all(City)
         if dictionary:
-            for k, v  in dictionary.items():
+            for k, v in dictionary.items():
                 if self.id == v.state_id:
-                    list_cities.append(v)
-        return list_cities
+                    cities_list.append(v)
+        return cities_list
