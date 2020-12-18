f7983f73bd48b09165178ba82ca852248151a53c
root <santmendieta@icloud.com>
2020-12-17 23:39:44 -0500

    Revert "4th refactor"
    This reverts commit 2dc0ab80c1c9e417e933fa865f34e6fecb59da42.

--- models/city.py
+++ models/city.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
+import os
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship, backref
-from os import getenv
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """city class, state ID and name  """
     __tablename__ = "cities"
+
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        state = relationship("State", backref="cities")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities', cascade="delete")
