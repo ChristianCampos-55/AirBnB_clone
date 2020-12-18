143fbc70e65004550a9a7732019fc0b3a65e4dc2
root <santmendieta@icloud.com>
2020-12-17 19:54:56 -0500

    Revert "review"
    This reverts commit 81becb079585d3be7e1e44f9c9abf8161f560603.

--- models/review.py
+++ models/review.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
-from sqlalchemy.orm import relationship, backref
+from models.base_model import BaseModel
 
 
-class Review(BaseModel, Base):
+class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = "reviews"
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    user = relationship("User",
-                        backref=backref("reviews",
-                                        cascade="all, delete-orphan"))
-    place = relationship("Place",
-                         backref=backref("reviews",
-                                         cascade="all, delete-orphan"))
+    place_id = ""
+    user_id = ""
+    text = ""
