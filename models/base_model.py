7fa9cfd7c092de05bfa90b3a24deba9d1ba54a56
root <santmendieta@icloud.com>
2020-12-17 19:55:52 -0500

    Revert "engine db storaged changed"
    This reverts commit fe47866659d01a408dbeaa1584e4dca70d815bbb.

--- models/base_model.py
+++ models/base_model.py
@@ -37,8 +37,7 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return '[{}] ({}) {}'.format(type(self).__name__,
-                                     self.id, self.to_dict())
+        return '[{}] ({}) {}'.format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
