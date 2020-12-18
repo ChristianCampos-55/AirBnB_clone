f4e49b609b40c12ca3ea15b161ad6f12c72e6fe6
root <santmendieta@icloud.com>
2020-12-17 19:54:55 -0500

    Revert "review"
    This reverts commit 91fba88908e2cf10149aab9f6fe518a9eabf0a21.

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """module for amenities"""
-from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
-from sqlalchemy.orm import relationship, backref
 from models.base_model import BaseModel, Base
+from sqlalchemy import Integer, Float, String, Column, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from models.review import Review
+from sqlalchemy.schema import Table
 
 
 class Amenity(BaseModel, Base):
-    """ amenities """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-
-    place_amenities = relationship('Place', secondary="place_amenity",
-                                   backref='amenities', viewonly=False)
+    '''Class that modifies amenities'''
+    name = ""
