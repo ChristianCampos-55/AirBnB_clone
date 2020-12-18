8fd636311049df1a839ee5ae84cf49cc8fb75cd6
root <santmendieta@icloud.com>
2020-12-17 22:43:18 -0500

    Revert "init.py conditional added, working so far"
    This reverts commit 7d42ecc4596e47d6c3f6fe04eff55ed844c044ee.

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage or DB"""
+"""This module instantiates an object of class FileStorage"""
 import os
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
 
 
 if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
     storage = DBStorage()
 else:
     storage = FileStorage()
