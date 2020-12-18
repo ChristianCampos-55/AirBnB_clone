3f7602f5268be322b7e89b22905d5ab1a56114f3
root <santmendieta@icloud.com>
2020-12-17 21:55:21 -0500

    Revert "state.py conditional removed, init.py storage NOne removed"
    This reverts commit 83d0c31d0e6f1a18748a771b8874a484c9b2f891.

--- models/state.py
+++ models/state.py
@@ -8,18 +8,16 @@ from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class within the model"""
+    """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
 
     @property
     def cities(self):
-        """ getter method for cities"""
-        from models import storage
-        cities_list = []
+        """getter for cities object"""
+        my_list = []
         dictionary = storage.all(City)
-        if dictionary:
-            for k, v in dictionary.items():
-                if self.id == v.state_id:
-                    cities_list.append(v)
-        return cities_list
+        for key, value in dictionary.items():
+            if value.state_id == self.id:
+                my_list.append(value)
+        return my_list
