0f0ef86eab83aa836b03087654f86a4ef80d2e61
root <santmendieta@icloud.com>
2020-12-17 21:05:24 -0500

    City.py changed, requirements working so far

--- models/city.py
+++ models/city.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 
+from sqlalchemy import String, Column, ForeignKey
+from sqlalchemy.orm import relationship, backref
 from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
-from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
