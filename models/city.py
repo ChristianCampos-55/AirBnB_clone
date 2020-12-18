a2ee4f2fe5ebccf4d582b292766bf1628c63d25e
root <santmendieta@icloud.com>
2020-12-17 22:58:45 -0500

    debugging states restated

--- models/city.py
+++ models/city.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-
-from sqlalchemy import String, Column, ForeignKey
-from sqlalchemy.orm import relationship, backref
-from os import getenv
+import os
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """ The city class, contains state ID and name
+    Attributes:
+        name: input name
+        state_id: input state id
+    """
     __tablename__ = "cities"
+
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        state = relationship("State", backref="cities")
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities', cascade="delete")
